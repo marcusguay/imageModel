8645a946394866331268f1bc2523c6950e6cf4b4
marcusguay <55104472+marcusguay@users.noreply.github.com>
2023-05-28 01:05:51 -0700

    Add files via upload

--- model.js
+++ model.js
@@ -1,5 +1,3 @@
-
-
 const tf = require('@tensorflow/tfjs-node');
 const fs = require('fs');
 const trainFolderPath = "Images\\Train";
@@ -12,8 +10,17 @@ const testImages = [];
 const testLabels = [];
 const imgHeight = 150;
 const imgWidth = 150;
+var timesToRun = 1000;
+var model;
+const NUM_OUTPUTS = 648;
 
+// NUMBER OF CLASSES TO SELECT
+const NUM_INPUTS = 5;
+var INPUTS_TENSOR; 
+var OUTPUTS_TENSOR;
 
+// DETERMINES FIRST INDEX OF DATA THAT WILL BE CHOSEN
+var rand = Math.floor(Math.random() * NUM_OUTPUTS);
 
 
 
@@ -40,29 +47,73 @@ function GetTestData(){
 // } );
  }
 
+
+
+ // Selects arbitrary amount of data to load by storing bools in a map
+function SelectionMap(){
+const selectMap = new Map();
+var fileIndex = 0;
+
+var Imagefolder = fs.readdirSync(trainFolderPath);
+Imagefolder.forEach(folder => { 
+selectMap.set(fileIndex,false);
+
+  fileIndex = fileIndex + 1; 
+});
+
+var numberSelections = NUM_INPUTS;
+var randtemp = rand;
+
+
+
+
+while(numberSelections > 0){
+console.log("has been selected at index" + rand);
+selectMap.set(rand,true);
+randtemp = (randtemp + Math.floor(Math.random() * NUM_OUTPUTS)) % NUM_OUTPUTS;
+numberSelections--;
+// Makes sure 1 element of randomly selected data will be apart of next batch
+rand = randtemp;
+}
+
+
+
+return selectMap;
+}
+
+
+
  function GetTrainData(){
     var fileIndex = 0;
     var Imagefolder = fs.readdirSync(trainFolderPath);
+  
+   const selectMap = SelectionMap();
+
+
     Imagefolder.forEach(folder => { 
-         console.log(folder + " " + fileIndex);
+         //console.log(folder + " " + fileIndex);
          map.set(fileIndex,folder);
-         fileIndex = fileIndex + 1;
+   if(selectMap.get(fileIndex)){
+    console.log(folder + " " + fileIndex);
         let folderLocation = trainFolderPath.concat("\\", folder);
             var Images = fs.readdirSync(folderLocation);
                 
                     Images.forEach(imageLocation => {
                         let imagePath = folderLocation.concat("\\",imageLocation)
                       //  console.log(imagePath); 
+                     var rand = Math.floor(Math.random() * 2);
+                     //console.log(rand); 
+                     if(rand == 0){
                        var image = fs.readFileSync(imagePath);  
                        var tensor = tf.node.decodePng(image,1).expandDims();
+                    
                      //console.log(tensor);
                       trainImages.push(tensor);
                       trainLabels.push(fileIndex);
-                      
+                     }
                    }); 
-    } );
-         
-
+                  }
+                   fileIndex = fileIndex + 1; } );                     
  }
 
 
@@ -80,17 +131,15 @@ let results = await model.fit(INPUTS_TENSOR,OUTPUTS_TENSOR,
  {
 shuffle: true,
 validationSplit: 0.2,
-batchSize: 64,
-epochs: 50,
+batchSize: 128,
+epochs: 10,
 });
 
 
 
 
 
-
-
-
+await model.save('file://C:\Users\Administrator\app\model');
 
 
 INPUTS_TENSOR.dispose();
@@ -117,65 +166,91 @@ prediction2.print();
 console.log("predicted " + prediction2.squeeze().argMax());
 
 
+tensor.dispose();
+tensor2.dispose();
+
+
+if(timesToRun > 0){
+  runModel();
+  timesToRun--;
+   }
+
+}
+
+
+async function loadModel(){
+ model = await tf.loadLayersModel('file://C:\\Users\\Administrator\\app\\UsersAdministratorappmodel\\model.json');
+ console.log(model.summary());
+runModel();
 }
 
 
 
- // Main method
+function createModel(){
+  
+model = tf.sequential();
+
+model.add(tf.layers.dense({inputShape: [150,150,1], units: 32, activation: 'relu'}));
+  model.add(tf.layers.flatten());
+  //model.add(tf.layers.dropout({ rate: 0.2 }));
+model.add(tf.layers.dense({units: 16, activation: 'relu'}));
+//model.add(tf.layers.dropout({ rate: 0.2 }));
+model.add(tf.layers.batchNormalization());
+model.add(tf.layers.dropout({ rate: 0.5 }));
+model.add(tf.layers.dense({units: NUM_OUTPUTS, activation: 'softmax'}));
+
+ // Pool data into a [2,2] matrix
+  // model.add(tf.layers.maxPooling2d({poolSize: [2,2]}));
+  // // Randomly selects neurons to omit 
+  // model.add(tf.layers.dropout({rate: 0.2}));
+  // model.add(tf.layers.flatten());
+  // model.add(tf.layers.dense({units: 12, activation: 'relu'}));
+  // model.add(tf.layers.dropout({rate: 0.3}));
+  // model.add(tf.layers.dense({units: 2, activation: 'sigmoid'}));
+
+
+ console.log(model.summary());
+
+runModel(); 
+}
+
+
+function runModel(){
+
 
  GetTrainData();
- //GetTestData();
 const INPUTS = trainImages;
 const OUTPUTS = trainLabels;
 
 
 // Shuffle both Inputs and Outputs (making sure indices match)
-tf.util.shuffleCombo(INPUTS,OUTPUTS);
+//tf.util.shuffleCombo(INPUTS,OUTPUTS);
 
 
 
 // Put all tensors into a single tensor as the input
-const INPUTS_TENSOR = tf.concat(trainImages); 
+INPUTS_TENSOR = tf.concat(trainImages); 
 
 // Put all
-const OUTPUTS_TENSOR = tf.oneHot(tf.tensor1d(OUTPUTS,'int32'),2);
+ OUTPUTS_TENSOR = tf.oneHot(tf.tensor1d(OUTPUTS,'int32'),NUM_OUTPUTS);
 
 
 
-//console.log(INPUTS_TENSOR);
-console.log(OUTPUTS_TENSOR);
 
-const model = tf.sequential();
-
-model.add(tf.layers.conv2d({
-    inputShape: [150, 150, 1],
-    filters: 32,
-    kernelSize: [3,3],
-    activation: 'relu',
-  }));
- // Pool data into a [2,2] matrix
-  model.add(tf.layers.maxPooling2d({poolSize: [2,2]}));
-  // Randomly selects neurons to omit 
-  model.add(tf.layers.dropout({rate: 0.2}));
-  model.add(tf.layers.flatten());
-  model.add(tf.layers.dense({units: 12, activation: 'relu'}));
-  model.add(tf.layers.dropout({rate: 0.3}));
-  model.add(tf.layers.dense({units: 2, activation: 'softmax'}));
- 
-
-  model.summary();
-
-  model.compile({
-    optimizer: tf.train.adam(),
-    loss: 'categoricalCrossentropy',
-    metrics : ['accuracy']
-});
-
-
+  
+    model.compile({
+      optimizer: tf.train.adam(), 
+      loss: 'categoricalCrossentropy', 
+      metrics: ['accuracy'] 
+    });
 
 
 train();
-var testData = GetTestData();
+  }
 
 
 
+
+   // MAIN METHOD (nice one js)
+  loadModel();
+  //createModel();
