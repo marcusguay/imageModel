a40fb3e8e51a9fd339aac92aacbb5a0f6b437d3a
marcusguay <55104472+marcusguay@users.noreply.github.com>
2023-06-03 10:38:15 -0700

    Add files via upload

--- model.py
+++ model.py
@@ -16,29 +16,43 @@ print(tf.config.list_physical_devices('GPU'))
 
 img_height = 150
 img_width = 150
-batch_size = 1024
-TIMES_TO_RUN = 1
+batch_size = 32
+TIMES_TO_RUN = 10
+NUM_IMG = 0
 
 data_dir = "C:\\Users\\Administrator\\app\\Images\\Train"
 
 INPUT_TENSOR = None
 OUTPUT_TENSOR = None
-NUM_OUTPUTS = 2
+NUM_OUTPUTS = 10
 
-tf.config.experimental.set_lms_defrag_enabled(True)
 
-model = tf.keras.Sequential(
-[
-tf.keras.Input(shape=(150,150,1)),  
-tf.keras.layers.Dense(64, activation = 'relu'),
-tf.keras.layers.Flatten(),
-tf.keras.layers.Dropout(0.2),
-tf.keras.layers.Dense(32, activation = 'relu'),
-tf.keras.layers.Dropout(0.2),
-tf.keras.layers.Dense(NUM_OUTPUTS, activation = 'softmax')
-]
+
+model = tf.keras.Sequential()
+
+model.add(layers.Conv2D(64, (3,3), activation='relu', input_shape= (img_width,img_height,1)))
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D((3,3)))
+model.add(layers.Dropout(0.2))
+model.add(layers.Conv2D(128, (3,3), activation='relu'))
+model.add(layers.Conv2D(128, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D((3,3)))
+model.add(layers.Dropout(0.2))
+model.add(layers.Flatten())
+model.add(layers.Dense(NUM_OUTPUTS, activation='softmax'))
+
+
+sgd = tf.keras.optimizers.SGD(
+    learning_rate=1e-5,
+    momentum=0.9,
+    decay=1e-6,
+
 )
-
+model.compile(
+optimizer = sgd,
+loss = tf.keras.losses.CategoricalCrossentropy(),
+metrics = 'accuracy'
+)
 
 
 
@@ -50,21 +64,18 @@ def show(image, label):
    plt.show()
 
 
-def processImage(path):
+def processImage(path,label):
    
 #     # print("processing iamge")
-#      #print(path)
+      #print(path)
       image =  tf.io.read_file(path)
       image = tf.io.decode_png(image,channels = 1, dtype = tf.dtypes.uint8)
       image = tf.image.convert_image_dtype(image, dtype = tf.int8 , saturate=False, name=None)
-
-      #  show(image, "Lol")
       image = tf.reshape(image,[img_width, img_height, 1])
-     #  label = tf.one_hot(label,NUM_OUTPUTS)
-     # label = tf.reshape(label,[NUM_OUTPUTS])
-     
+      label = tf.one_hot(label,NUM_OUTPUTS,dtype = tf.float16)
+      print(image)
 
-      return image
+      return image,label
   
 
 
@@ -78,18 +89,18 @@ InputList = []
 import os
 
 for dirname in os.listdir(data_dir):
- if(NUM_IMG < 20):
      path = os.path.join(data_dir,dirname)
    
      #print("directory")
-     print(dirname)
+     print(dirname + str(LABEL))
      for filename in os.listdir(path):
          # print("file")
         # print(os.path.join(dirname,filename))
         # processImage(os.path.join(dirname,filename),LABEL)
-         InputList.append(processImage(os.path.join(data_dir,os.path.join(dirname,filename))))
-         OutputList.append(tf.one_hot(LABEL,NUM_OUTPUTS))
+         InputList.append(os.path.join(data_dir,os.path.join(dirname,filename)))
+         OutputList.append(LABEL)
          NUM_IMG = NUM_IMG + 1
+         
 
      if DATASET == None:
       TEMP_INPUT_TENSOR = tf.convert_to_tensor(InputList)
@@ -106,19 +117,22 @@ for dirname in os.listdir(data_dir):
       OutputList = []
      LABEL = LABEL + 1
      
+     
 
  
      
+DATASET = DATASET.shuffle(NUM_IMG,reshuffle_each_iteration=True)
 
+DATASET= DATASET.map(processImage)
 
+NUM_OUTPUTS = 648
 
-
-DATASET = DATASET.shuffle(NUM_OUTPUTS)
 Split =  (int) (NUM_IMG // (10/2))
+print(Split)
 TEST_SET = DATASET.take(Split)
 BATCH_DATASET = DATASET.skip(Split)
-BATCH_DATASET = BATCH_DATASET.batch(2)
-TEST_SET = TEST_SET.batch(2)
+BATCH_DATASET = BATCH_DATASET.batch(batch_size)
+TEST_SET = TEST_SET.batch(batch_size)
 
 
 
@@ -131,26 +145,20 @@ TEST_SET = TEST_SET.batch(2)
 
 
 # print(DATASET)
-#print(BATCH_DATASET)
-# print(TEST_SET)
+# print(BATCH_DATASET)
+#  print(TEST_SET)
 
-# print( "test")
+print(NUM_IMG)
 
 
 #print("batch")
-
-# for element in DATASET:
-#  print(element)
-
-
+# 
+# for element in TEST_SET:
+#    print(element)
 
 
 
-model.compile(
-optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3),
-loss = tf.keras.losses.CategoricalCrossentropy(),
-metrics = 'accuracy'
-)
+
 
 early_stopping_monitor = EarlyStopping(
     monitor='accuracy',
@@ -167,7 +175,7 @@ while (TIMES_TO_RUN > 0):
     model.fit(
        BATCH_DATASET, 
        validation_data= TEST_SET,
-    epochs = 1,
+    epochs = 10,
     shuffle = True,
    callbacks = [early_stopping_monitor]
    )
